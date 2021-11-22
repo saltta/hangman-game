68e93b0f3db5a2971e4230d209f642f8388dc5d3
saltta <trottsal@gmail.com>
2021-11-22 22:46:52 +0000

    started game function and set the word to be guessed variable

--- run.py
+++ run.py
@@ -4,3 +4,11 @@ with open('words.txt', 'r') as h:
     words = h.readlines()
 
 word = random.choice(words)[:-1].upper()
+
+
+def start_game(word):
+    """
+    Starts the game.
+    """
+    secret_word = "_" * len(word)
+    
