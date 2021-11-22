7ea551b684514ea4e2fa1b84798c8452de118fb2
saltta <trottsal@gmail.com>
2021-11-22 23:15:40 +0000

    separated characters to be guessed to make it visible how many letters are in the secret word

--- run.py
+++ run.py
@@ -14,9 +14,9 @@ def start_game(word):
     endgame = False
     guessed_letters = []
     chances = 6
-    print("Let's play Hangman!")
-    print(f"Chances left: {chances}")
-    print(secret_word)
-    print("\n")
+    print("Let's play Hangman!\n")
+    print(f"Chances left: {chances}\n")
+    print("Guess this word: " + " ".join(secret_word) + "\n")
+
 
 start_game(word)
